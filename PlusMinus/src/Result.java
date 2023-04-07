8a45a133d051cb1a09792f7ec15e5a017f579abe
Aspink <aspink1@wp.pl>
2023-04-07 14:41:14 +0200

    PlusMinus#04 Class Result with starting content.

--- PlusMinus/src/Result.java
+++ PlusMinus/src/Result.java
@@ -1,2 +1,21 @@
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import static java.util.stream.Collectors.joining;
+import java.util.*;
 public class Result {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+        // Write your code here
+
+    }
 }
