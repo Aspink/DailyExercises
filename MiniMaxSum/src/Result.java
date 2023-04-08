105a7dff2aad6a32678abea826cac3294156a981
Aspink <aspink1@wp.pl>
2023-04-08 19:41:11 +0200

    MiniMaxSum#06 Method miniMaxSum finished for Integer.

--- MiniMaxSum/src/Result.java
+++ MiniMaxSum/src/Result.java
@@ -1,3 +1,15 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
 public class Result {
 
     /*
@@ -7,7 +19,18 @@ public class Result {
      */
 
     public static void miniMaxSum(List<Integer> arr) {
-        // Write your code here
-
+        Integer sum = 0;
+        Integer xMin = arr.get(0);
+        Integer xMax = xMin;
+        for(Integer x : arr) {
+            if(x < xMin) {
+                xMin = x;
+            }
+            if(x > xMax) {
+                xMax = x;
+            }
+            sum += x;
+        }
+        System.out.println((sum - xMax) + " "  + (sum - xMin));
     }
 }
