d0d09d1098f9e81588425eb0fbb1aea2b82be62b
Aspink <aspink1@wp.pl>
2023-04-08 19:17:48 +0200

    MiniMaxSum#04 Method miniMaxSum drafted.

--- MiniMaxSum/src/Result.java
+++ MiniMaxSum/src/Result.java
@@ -1,2 +1,13 @@
 public class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        // Write your code here
+
+    }
 }
