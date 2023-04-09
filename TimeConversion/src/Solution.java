bb1f18c958bce4c4ee7305c04bf1488c28924045
Aspink <aspink1@wp.pl>
2023-04-09 20:47:00 +0200

    TimeConversion#03 Solution class finished.

--- TimeConversion/src/Solution.java
+++ TimeConversion/src/Solution.java
@@ -1,5 +1,16 @@
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
