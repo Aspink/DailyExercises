7696104ac4047ecdc5e2fbf41e1cd74981b76312
Aspink <aspink1@wp.pl>
2023-04-10 19:29:07 +0200

    Median#03 Result class finished.

--- Median/src/Result.java
+++ Median/src/Result.java
@@ -1,2 +1,31 @@
+import java.util.List;
+
 public class Result {
+
+    /*
+     * Complete the 'findMedian' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int findMedian(List<Integer> arr) {
+        for(int i = 0; i < arr.size(); i++) {
+            int lover = 0;
+            int higher = 0;
+            for(int j = 0; j < arr.size(); j++) {
+                if(arr.get(i) >= arr.get(j)) {
+                    higher++;
+                } else {
+                    lover++;
+                }
+            }
+            higher--;
+            if(higher == lover) {
+                return arr.get(i);
+            }
+        }
+        return 0;
+
+    }
 }
