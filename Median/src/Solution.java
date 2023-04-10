85cc6f8c4b4361615c994fbb395ae383a011785d
Aspink <aspink1@wp.pl>
2023-04-10 19:25:56 +0200

    Median#01 Module & Solution class created.

---
+++ Median/src/Solution.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'findMedian' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int findMedian(List<Integer> arr) {
+        for(int i = 0; i < arr.size(); i++) {
+            int lover = 0;
+            int higher = 0;
+            for(int j = 0; j < arr.size(); j++) {
+                if(arr.get(i) >= arr.get(j)) {
+                    higher++;
+                } else {
+                    lover++;
+                }
+                higher--;
+                if(higher == lover) {
+                    return arr.get(i);
+                }
+                return 0;
+            }
+        }
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.findMedian(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
