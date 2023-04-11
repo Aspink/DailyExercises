f650fa3592f7995e572778ad1cdcfa389009aad7
Aspink <aspink1@wp.pl>
2023-04-11 18:58:53 +0200

    LonelyInteger#04 Method lonelyInteger finished.

--- LonelyInteger/src/Process.java
+++ LonelyInteger/src/Process.java
@@ -1,2 +1,10 @@
+import java.util.List;
+
 public class Process {
+    public static int lonelyInteger(List<Integer> a) {
+        Integer result = a.stream()
+                .filter(i -> a.indexOf(i) ==  a.lastIndexOf(i))
+                .findFirst().get();
+        return result;
+    }
 }
