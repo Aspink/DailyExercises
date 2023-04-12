ea33f2155cc8bc76d630f2b24778afc72718484d
Aspink <aspink1@wp.pl>
2023-04-12 13:16:02 +0200

    DiagonalDifference#02 Class Main finished.

--- DiagonalDifference/src/Main.java
+++ DiagonalDifference/src/Main.java
@@ -1,5 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = DiaDif.diagonalDifference(arr);
+
+        System.out.println(result);
+
+        bufferedReader.close();
     }
 }
