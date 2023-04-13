0b8a21ee06ea2c313bb6f71ec3e963c2442b25b4
Aspink <aspink1@wp.pl>
2023-04-13 21:21:19 +0200

    CountingSort1#07 Method countingSort corrected.

--- CountingSort1/src/Sorter.java
+++ CountingSort1/src/Sorter.java
@@ -1,14 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sorter {
-
-    /*
-     * Complete the 'countingSort' function below.
-     *
-     * The function is expected to return an INTEGER_ARRAY.
-     * The function accepts INTEGER_ARRAY arr as parameter.
-     */
-
     public static List<Integer> countingSort(List<Integer> arr) {
-        // Write your code here
-
+        List<Integer> count = new ArrayList<>();
+        for(Integer i = 0; i < 100; i++) {
+            count.add(0);
+        }
+        for(Integer number : arr) {
+            count.set(number, count.get(number) + 1);
+        }
+        return count;
     }
 }
