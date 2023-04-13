f4fd3c6b352e01a109258bc3ef03eb6cf9f7db54
Aspink <aspink1@wp.pl>
2023-04-13 21:43:44 +0200

    CountingSort1#09 Main class fixed. Logic tested.

--- CountingSort1/src/Main.java
+++ CountingSort1/src/Main.java
@@ -1,18 +1,11 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int n = Integer.parseInt(bufferedReader.readLine().trim());
 
@@ -22,7 +15,7 @@ public class Main {
 
         List<Integer> result = Sorter.countingSort(arr);
 
-        bufferedWriter.write(
+        System.out.println(
                 result.stream()
                         .map(Object::toString)
                         .collect(joining(" "))
@@ -30,6 +23,5 @@ public class Main {
         );
 
         bufferedReader.close();
-        bufferedWriter.close();
     }
 }
