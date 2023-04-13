a841954309e02296d5178d75f38a07e75d1ac8ad
Aspink <aspink1@wp.pl>
2023-04-13 21:09:21 +0200

    CountingSort1#05 Sorter class sketch.

--- CountingSort1/src/Sorter.java
+++ CountingSort1/src/Sorter.java
@@ -1,2 +1,14 @@
 public class Sorter {
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+        // Write your code here
+
+    }
 }
