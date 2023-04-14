c165a5d4a42a0efb7530c0d6a6afbc40324e73e8
Aspink <aspink1@wp.pl>
2023-04-14 20:55:02 +0200

    FlippingTheMatrix#03 Class MatrixFlipper sketch.

--- FlippingTheMatrix/src/MatrixFlipper.java
+++ FlippingTheMatrix/src/MatrixFlipper.java
@@ -1,2 +1,15 @@
 public class MatrixFlipper {
+
+    /*
+     * Complete the 'flippingMatrix' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+     */
+
+    public static int flippingMatrix(List<List<Integer>> matrix) {
+        // Write your code here
+
+    }
+
 }
