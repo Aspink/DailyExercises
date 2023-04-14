d57d9c4b72551be37ded6d9fdacbdee9ce6100b4
Aspink <aspink1@wp.pl>
2023-04-14 20:16:44 +0200

    FizzBuzz#04 Methof main created.

--- FizzBuzz/src/Main.java
+++ FizzBuzz/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.fizzBuzz(n);
+
+        bufferedReader.close();
     }
 }
