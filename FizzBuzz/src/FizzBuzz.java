6604179bff7c02fed989b9ec8b93d54aac2121f8
Aspink <aspink1@wp.pl>
2023-04-14 20:15:19 +0200

    FizzBuzz#03 Methof fizzBuzz created.

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -1,2 +1,18 @@
 public class FizzBuzz {
+    public static void fizzBuzz(int n) {
+        for(int i = 1; i <= n; i++){
+            String output = "";
+            if(i%3 == 0) {
+                output = output.concat("Fizz");
+            }
+            if(i%5 == 0) {
+                output = output.concat("Buzz");
+            }
+            if(output.length() == 0) {
+                output = output.concat(String.valueOf(i));
+            }
+            System.out.println(output);
+        }
+
+    }
 }
