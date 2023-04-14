9ffbe9fb118aaa90061f71ed3c1233457d76b019
Aspink <aspink1@wp.pl>
2023-04-14 20:17:58 +0200

    FizzBuzz#05 Import added, main corrected.

--- FizzBuzz/src/Main.java
+++ FizzBuzz/src/Main.java
@@ -1,10 +1,12 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-        Result.fizzBuzz(n);
+        FizzBuzz.fizzBuzz(n);
 
         bufferedReader.close();
     }
