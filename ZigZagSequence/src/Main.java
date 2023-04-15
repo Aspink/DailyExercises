1833f4cbf5c365f7633908dfe30c46a27e6ff49f
Aspink <aspink1@wp.pl>
2023-04-15 18:33:58 +0200

    ZigZagSequence#03 Class Main corrected.

--- ZigZagSequence/src/Main.java
+++ ZigZagSequence/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.*;
+import java.lang.*;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner kb = new Scanner(System.in);
+        int test_cases = kb.nextInt();
+        for(int cs = 1; cs <= test_cases; cs++){
+            int n = kb.nextInt();
+            int a[] = new int[n];
+            for(int i = 0; i < n; i++){
+                a[i] = kb.nextInt();
+            }
+            ZigZagSequence.findZigZagSequence(a, n);
+        }
     }
 }
