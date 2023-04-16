5e6d2042684443e2999de3e36a36cd368d361760
Aspink <aspink1@wp.pl>
2023-04-16 21:03:22 +0200

    TowerBreakers#05 TowerBreakers Class sketched.

--- TowerBreakers/src/TowerBreakers.java
+++ TowerBreakers/src/TowerBreakers.java
@@ -1,2 +1,16 @@
 public class TowerBreakers {
+
+    /*
+     * Complete the 'towerBreakers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER m
+     */
+
+    public static int towerBreakers(int n, int m) {
+        // Write your code here
+
+    }
 }
