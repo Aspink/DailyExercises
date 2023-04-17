53d2b2ddf212abc06d0cf3819eb394ff6f1bffcc
Aspink <aspink1@wp.pl>
2023-04-17 14:42:49 +0200

    CaesarCipher#02 Method main created.

--- CaesarCipher/src/Main.java
+++ CaesarCipher/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        String s = bufferedReader.readLine();
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+        String result = CaesarsCode.caesarCipher(s, k);
+        System.out.println(result);
+        bufferedReader.close();
     }
 }
