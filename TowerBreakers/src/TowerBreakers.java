f037cc9cc4ac0fd8a92dcf8acd63cf819367f511
Aspink <aspink1@wp.pl>
2023-04-17 12:58:36 +0200

    TowerBreakers#06 TowerBreakers Class & towerBreakers method finished.

--- TowerBreakers/src/TowerBreakers.java
+++ TowerBreakers/src/TowerBreakers.java
@@ -1,16 +1,8 @@
 public class TowerBreakers {
-
-    /*
-     * Complete the 'towerBreakers' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts following parameters:
-     *  1. INTEGER n
-     *  2. INTEGER m
-     */
-
     public static int towerBreakers(int n, int m) {
-        // Write your code here
-
+        if(n % 2 == 0 || m == 1) {
+            return 2;
+        }
+        return 1;
     }
 }
