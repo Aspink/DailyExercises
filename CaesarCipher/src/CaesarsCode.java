18aa6e411c43500fbb9486c3c117909f914527cf
Aspink <aspink1@wp.pl>
2023-04-17 15:13:54 +0200

    CaesarCipher#05 Method caesarCipher constructed.

--- CaesarCipher/src/CaesarsCode.java
+++ CaesarCipher/src/CaesarsCode.java
@@ -1,2 +1,18 @@
 public class CaesarsCode {
+    public static String caesarCipher(String s, int k) {
+        StringBuilder string = new StringBuilder();
+        char[] chars = s.toCharArray();
+        for(char letter : chars) {
+            if(Character.isLetter(letter)) {
+                if(letter >= 'a' && letter <= 'z') {
+                    letter = (char) (((letter + k - 'a') % 26) + 'a');
+                }
+                if(letter >= 'A' && letter <= 'Z') {
+                    letter = (char) (((letter + k - 'A') % 26) + 'A');
+                }
+            }
+            string.append(letter);
+        }
+        return string.toString();
+    }
 }
