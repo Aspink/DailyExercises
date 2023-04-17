287636e0a96c4fe6a3f7ad454558315fd54132df
Aspink <aspink1@wp.pl>
2023-04-17 13:02:27 +0200

    TowerBreakers#06 Method main corrected.

--- TowerBreakers/src/Main.java
+++ TowerBreakers/src/Main.java
@@ -4,10 +4,7 @@ import java.util.stream.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
         int t = Integer.parseInt(bufferedReader.readLine().trim());
-
         IntStream.range(0, t).forEach(tItr -> {
             try {
                 String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
@@ -18,14 +15,12 @@ public class Main {
 
                 int result = TowerBreakers.towerBreakers(n, m);
 
-                bufferedWriter.write(String.valueOf(result));
-                bufferedWriter.newLine();
+                System.out.println(result);
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
         });
 
         bufferedReader.close();
-        bufferedWriter.close();
     }
 }
