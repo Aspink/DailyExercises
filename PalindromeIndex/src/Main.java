d184478c04e66372afe29e377d66a8fabde1efde
Aspink <aspink1@wp.pl>
2023-04-18 21:01:00 +0200

    PalindromeIndex#06 Method main corrected.

--- PalindromeIndex/src/Main.java
+++ PalindromeIndex/src/Main.java
@@ -4,24 +4,19 @@ import java.util.stream.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int q = Integer.parseInt(bufferedReader.readLine().trim());
 
         IntStream.range(0, q).forEach(qItr -> {
             try {
                 String s = bufferedReader.readLine();
-
-                int result = Result.palindromeIndex(s);
-
-                bufferedWriter.write(String.valueOf(result));
-                bufferedWriter.newLine();
+                int result = PalindromeIndex.palindromeIndex(s);
+                System.out.println(result);
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
         });
 
         bufferedReader.close();
-        bufferedWriter.close();
     }
 }
