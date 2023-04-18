d131ace2906c23476d591551ca9560192411a5c7
Aspink <aspink1@wp.pl>
2023-04-18 20:18:42 +0200

    PalindromeIndex#03 Imports in Main class added.

--- PalindromeIndex/src/Main.java
+++ PalindromeIndex/src/Main.java
@@ -1,3 +1,5 @@
+mport java.io.*;
+import java.util.stream.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
