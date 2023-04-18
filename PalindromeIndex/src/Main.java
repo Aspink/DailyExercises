5f338fad11b7a95cf2ca6edc60c65f72fd230d1a
Aspink <aspink1@wp.pl>
2023-04-18 20:20:46 +0200

    PalindromeIndex#04 PalindromeIndex class started.

--- PalindromeIndex/src/Main.java
+++ PalindromeIndex/src/Main.java
@@ -1,5 +1,6 @@
-mport java.io.*;
+import java.io.*;
 import java.util.stream.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
