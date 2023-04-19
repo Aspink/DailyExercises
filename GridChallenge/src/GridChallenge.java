81888396cb053d5333f7ba9215543f383eea1d85
Aspink <aspink1@wp.pl>
2023-04-19 15:59:20 +0200

    GridChallenge#04 Method GridChallenge started.

--- GridChallenge/src/GridChallenge.java
+++ GridChallenge/src/GridChallenge.java
@@ -1,2 +1,9 @@
+import java.util.List;
+
 public class GridChallenge {
+    public static String gridChallenge(List<String> grid) {
+        String answer = "YES";
+
+        return answer;
+    }
 }
