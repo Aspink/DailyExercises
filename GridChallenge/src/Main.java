82809b41e99694227dd23c74eccae49c179022dc
Aspink <aspink1@wp.pl>
2023-04-19 15:54:51 +0200

    GridChallenge#03 Method main constructed.

--- GridChallenge/src/Main.java
+++ GridChallenge/src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<String> grid = IntStream.range(0, n).mapToObj(i -> {
+                            try {
+                                return bufferedReader.readLine();
+                            } catch (IOException ex) {
+                                throw new RuntimeException(ex);
+                            }
+                        })
+                        .collect(toList());
+
+                String result = GridChallenge.gridChallenge(grid);
+
+                System.out.println(result);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
     }
 }
