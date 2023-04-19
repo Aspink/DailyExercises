28aeee8c7ba37b288efe2e24e2a6100849177b22
Aspink <aspink1@wp.pl>
2023-04-19 19:13:13 +0200

    GridChallenge#05 Methods gridChallenge & sort finished.

--- GridChallenge/src/GridChallenge.java
+++ GridChallenge/src/GridChallenge.java
@@ -1,9 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GridChallenge {
     public static String gridChallenge(List<String> grid) {
         String answer = "YES";
-
+        List<String> newGrid = new ArrayList<>();
+        for(String row : grid) {
+            newGrid.add(sort(row));
+        }
+        for(int i = 0; i <  grid.get(0).length(); i++) {
+            for(int j = 0; j < grid.size() - 1; j++) {
+                if(newGrid.get(j).charAt(i) > newGrid.get(j + 1).charAt(i)) {
+                    return "NO";
+                }
+            }
+        }
         return answer;
+    }
+    public static String sort(String string) {
+        char[] chars = string.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
     }
 }
