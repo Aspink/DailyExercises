e3f4cdb8fe5378046127e01f152f812010225be5
Aspink <aspink1@wp.pl>
2023-04-20 21:23:26 +0200

    RecursiveDigitSum#08 Method singleSuperDigit build.

--- RecursiveDigitSum/src/RecursiveDigitSum.java
+++ RecursiveDigitSum/src/RecursiveDigitSum.java
@@ -1,8 +1,21 @@
+import java.math.BigInteger;
+
 public class RecursiveDigitSum {
     public static int superDigit(String n, int k) {
+        n = singleSuperDigit(n);
+        System.out.println(n);
         return 1;
     }
     public static String singleSuperDigit(String s) {
+        char[] chars = s.toCharArray();
+        BigInteger sum = BigInteger.ZERO;
+        for(char ch : chars) {
+            sum.add(BigInteger.valueOf(Character.getNumericValue(ch)));
+        }
+        s = sum.toString();
+        if(s.length() > 1) {
+            s = singleSuperDigit(s);
+        }
         return s;
     }
 }
