8c4362c9ce1d130aa5706dd843f82ecfc282e893
Aspink <aspink1@wp.pl>
2023-04-20 20:58:07 +0200

    RecursiveDigitSum#04 Import added.

--- RecursiveDigitSum/src/Main.java
+++ RecursiveDigitSum/src/Main.java
@@ -1,5 +1,22 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        String n = firstMultipleInput[0];
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.superDigit(n, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
