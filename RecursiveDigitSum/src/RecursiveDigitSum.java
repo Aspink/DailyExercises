b366ad80e16d1eb0200568c2266478cb13416d79
Aspink <aspink1@wp.pl>
2023-04-20 21:40:38 +0200

    RecursiveDigitSum#10 Method superDigit finished & tested.

--- RecursiveDigitSum/src/RecursiveDigitSum.java
+++ RecursiveDigitSum/src/RecursiveDigitSum.java
@@ -2,8 +2,12 @@ import java.math.BigInteger;
 
 public class RecursiveDigitSum {
     public static int superDigit(String n, int k) {
+        String s = "";
         n = singleSuperDigit(n);
-        System.out.println(n);
+        for(int i = 1; i <= k; i++) {
+            s = s.concat(n);
+        }
+        n = singleSuperDigit(s);
         return Character.getNumericValue(n.charAt(0));
     }
     public static String singleSuperDigit(String s) {
@@ -13,7 +17,6 @@ public class RecursiveDigitSum {
             sum = sum.add(BigInteger.valueOf(Character.getNumericValue(ch)));
         }
         s = sum.toString();
-        System.out.println(s);
         if(s.length() > 1) {
             s = singleSuperDigit(s);
         }
