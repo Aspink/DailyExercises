01273d56f23227de1eadd7c79f2c11bcef9f445e
Aspink <aspink1@wp.pl>
2023-04-20 21:05:45 +0200

    RecursiveDigitSum#07 Method singleSuperDigit created.

--- RecursiveDigitSum/src/RecursiveDigitSum.java
+++ RecursiveDigitSum/src/RecursiveDigitSum.java
@@ -2,4 +2,7 @@ public class RecursiveDigitSum {
     public static int superDigit(String n, int k) {
         return 1;
     }
+    public static String singleSuperDigit(String s) {
+        return s;
+    }
 }
