40ff0f1538b5008d8d7ac9cd426e753dfadb02f5
Aspink <aspink1@wp.pl>
2023-04-21 20:23:57 +0200

    NewYearChaos#05 Method minimumBribes created.

--- NewYearChaos/src/NewYearChaos.java
+++ NewYearChaos/src/NewYearChaos.java
@@ -1,2 +1,14 @@
 public class NewYearChaos {
+
+    /*
+     * Complete the 'minimumBribes' function below.
+     *
+     * The function accepts INTEGER_ARRAY q as parameter.
+     */
+
+    public static void minimumBribes(List<Integer> q) {
+        // Write your code here
+
+    }
+
 }
