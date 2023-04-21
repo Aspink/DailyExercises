2b08d25999b7935521eb490348dd231df7662702
Aspink <aspink1@wp.pl>
2023-04-21 21:17:52 +0200

    NewYearChaos#08 New version of method minimumBribes created.

--- NewYearChaos/src/NewYearChaos.java
+++ NewYearChaos/src/NewYearChaos.java
@@ -3,12 +3,16 @@ import java.util.List;
 public class NewYearChaos {
     public static void minimumBribes(List<Integer> q) {
         int bribes = 0;
-        for(int i = 0; i < q.size(); i++) {
-            int personBribes = q.get(i) - i;
-            if(personBribes < 0) personBribes = 0;
-            if(personBribes > 2) {
-                System.out.println("Too chaotic");
-                return;
+        for(int i = 0; i < q.size() - 1; i++) {
+            int personBribes = 0;
+            for(int j = i + 1; j < q.size(); j++) {
+                if(q.get(i) > q.get(j)) {
+                    personBribes++;
+                    if(personBribes > 2) {
+                        System.out.println("Too chaotic");
+                        return;
+                    }
+                }
             }
             bribes += personBribes;
         }
