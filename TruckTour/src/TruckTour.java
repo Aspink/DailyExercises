b49f51ae3aa632973a3ea54571f77b8d15a8d805
Aspink <aspink1@wp.pl>
2023-04-22 13:57:45 +0200

    TruckTour#06 Method TruckTour constructed.

--- TruckTour/src/TruckTour.java
+++ TruckTour/src/TruckTour.java
@@ -2,7 +2,25 @@ import java.util.List;
 
 public class TruckTour {
     public static int truckTour(List<List<Integer>> petrolpumps) {
-        // Write your code here
+        boolean passed = false;
+        int i = 0;
+        while(!passed) {
+            int fuel = 0;
+            passed = true;
+            for(List<Integer> pump : petrolpumps) {
+                fuel += pump.get(0);
+                if(fuel < pump.get(1)) {
+                    passed =  false;
+                    petrolpumps.add(petrolpumps.get(0));
+                    petrolpumps.remove(0);
+                    i++;
+                    break;
+                }
+            }
+
+
+        }
+        return  i;
 
     }
 }
