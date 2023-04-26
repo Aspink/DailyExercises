28195fe9784f8c6132bfd8038a596573cec3ea84
Aspink <aspink1@wp.pl>
2023-04-26 19:36:59 +0200

    MergeLinkedLists#11 mergeList reconstructed & fail test again.

--- MergeLinkedLists/src/Main.java
+++ MergeLinkedLists/src/Main.java
@@ -61,7 +61,7 @@ public class Main {
         SinglyLinkedList list = new SinglyLinkedList();
         SinglyLinkedListNode current1 = head1;
         SinglyLinkedListNode current2 = head2;
-        while (!current1.next.equals(null)&&!current2.next.equals(null)){
+        while (list.tail.next != null){
             if(current1.data > current2.data) {
                 list.insertNode(current2.data);
                 current2 = current2.next;
