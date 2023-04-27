6a685ecaa8cddda85b18c67ee5e089537fe06b90
Aspink <aspink1@wp.pl>
2023-04-27 17:44:32 +0200

    QueueUsingTwoStacks#3 Method main created.

--- QueueUsingTwoStacks/src/Solution.java
+++ QueueUsingTwoStacks/src/Solution.java
@@ -1,5 +1,6 @@
 public class Solution {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
     }
 }
